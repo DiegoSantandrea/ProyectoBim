bf5c8a754582669f96539f952dfcac3b5826f629
santandrea <diegotata2006@gmail.com>
2025-03-04 17:36:08 -0600

    se agregaron los archivos necesarios al inicar el pryecto

---
+++ crs/helpers/db-validator.js
@@ -0,0 +1,30 @@
+import User from "../user/user.model.js";
+
+export const emailExists = async (email = "") => {
+    const existe = await User.findOne({email})
+    if(existe){
+        throw new Error(`The email ${email} is already registered`)
+    }
+}
+
+export const usernameExists = async (username = "") => {
+    const existe = await User.findOne({username})
+    if(existe){
+        throw new Error(`The username ${username} is already registered`)
+    }
+}
+
+export const userExists = async (uid = " ") => {
+    const existe = await User.findById(uid)
+    console.log(existe)
+    if(!existe){
+        throw new Error("No existe el usuario con el ID proporcionado")
+    }
+}
+
+export const telephoneExists = async (telephone = "") => {
+    const existe = await User.findOne({ telephone })
+    if (existe) {
+        throw new Error(`User: ${telephone}, is already registered`)
+    }
+}
